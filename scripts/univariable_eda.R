c5f1b878996133898ba9b3aa890972bc8a7d9593
Pedro Magalhães <pedro.magalhaes@mosaic.pt>
2022-03-28 10:40:07 +0100

    Added corrplot

--- scripts/univariable_eda.R
+++ scripts/univariable_eda.R
@@ -3,6 +3,7 @@
 
 library(tidyverse)
 library(readr)
+library(corrplot)
 
 source("scripts/eda_helper.R", local = TRUE, encoding = "UTF-8")
 
@@ -17,7 +18,7 @@ data <-
              escape_double = FALSE, 
              trim_ws = TRUE)
 
-## Get individual features names and type
+2## Get individual features names and type
 
 features_character <- 
   data %>%
@@ -41,3 +42,18 @@ eda_continuous <- map(features_continuous, ~eda_continuous(data, .x))
 eda_map <- c(eda_categorical, eda_continuous)
 
 rm(eda_categorical, eda_continuous)
+
+
+
+
+# Correlation plot --------------------------------------------------------
+
+# corr matrix
+
+corr_matrix <- 
+  cor(
+    dplyr::select( data, - ProjectCode, - BH_ID, - Motherhole ,- Date, - Stratigraphy, - HoleType ),
+    method = c("pearson"),
+    use = "complete.obs"
+    )
+
